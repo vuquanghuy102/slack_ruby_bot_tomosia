29db8596bd54fe59b5daf5460021e67bffd4e4a6
vuquanghuy102 <vuquanghuy001@gamil.com>
2019-06-14 13:34:09 +0700

    devise

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,6 @@
 class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :recoverable, :registerable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :rememberable, :validatable
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.destroy_all
+
+Admin.create!({
+  email: "admin@tomosia.com",
+  password: "admin@1234"
+})
